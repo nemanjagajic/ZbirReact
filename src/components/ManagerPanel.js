250ca5f5d671b6a6a12752311fc23702d9276c40
Nemanja Gajic <nemanja.gajic@pbssolutions.net>
2018-09-27 16:18:04 +0200

    logo added

--- src/components/ManagerPanel.js
+++ src/components/ManagerPanel.js
@@ -7,6 +7,10 @@ const ManagerPanel = () => (
 		<Header />
 		<div className="manager-content">
 			<div className="manager-content__left">
+				<div className="user-list"></div>
+				<div className="zbir-logo">
+					<img src="/images/zbir-logo.png" />
+				</div>
 			</div>
 			<div className="manager-content__right">
 				<div className="statistics">
